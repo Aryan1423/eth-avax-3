f1f02b91341f31e383b1a985498399d85808b514
Aryan Singh <113651343+Aryan1423@users.noreply.github.com>
2024-06-27 14:26:09 +0530

    Add files via upload

---
+++ aryanToken.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Token is ERC20, Ownable {
+    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) Ownable(msg.sender) {}
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);}
+
+ 
+    function burn(uint256 amount) external {
+        _burn(_msgSender(), amount);}
+}
